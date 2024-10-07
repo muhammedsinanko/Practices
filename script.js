76e5bcb23145afbb97dbbe8540e569565fbd0e6b
Muhammed sinan <sinansinu20021303@gmail.com>
2024-10-07 11:20:10 +0530

    css added

--- script.js
+++
@@ -1,9 +0,0 @@
-const card = document.getElementById('blog-card');
-const cardTitle = document.getElementById('card-title');
-card.addEventListener('mouseenter', () => {
-    cardTitle.style.color = '#f4d04e';
-    
-});
-card.addEventListener('mouseleave',() => {
-    cardTitle.style.color='black'
-})
